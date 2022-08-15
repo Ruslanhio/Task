437b532cf23e905ec77b6647455ed0c8024a8590
Ruslanhio <ruslanhio2891@gmail.com>
2022-08-15 19:36:41 +0300

    task15

--- task.js
+++ task.js
@@ -29,3 +29,14 @@ const HIGH_TEMPERATURE = {
   const {tomorrow: highTomorrow} = HIGH_TEMPERATURES;
   
   // Only change code above this line
+
+  const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Only change code below this line
+    
+  const {today: {low: lowsToday, high: highsToday }} = LOCAL_FORECAST;
+  // Only change code above this line
