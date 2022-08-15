752131f3fdfcd9743964c2432a71400162e2cdac
Ruslanhio <ruslanhio2891@gmail.com>
2022-08-15 22:06:54 +0300

    task29

--- task.js
+++ task.js
@@ -33,7 +33,7 @@ const HIGH_TEMPERATURE = {
   const LOCAL_FORECAST = {
     yesterday: { low: 61, high: 75 },
     today: { low: 64, high: 77 },
-    tomorrow: { low: 68, high: 80 }
+    tomorrow: { low: 68, high: 79 }
   };
   
   // Only change code below this line
