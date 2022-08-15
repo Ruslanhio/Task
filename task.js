c72411302544a9a17964bb9fcaf97ec2ac95b576
Ruslanhio <ruslanhio2891@gmail.com>
2022-08-15 17:06:46 +0300

    task11

--- task.js
+++ task.js
@@ -4,3 +4,15 @@ let arr3;
 arr3 = [...arr2];  // Change this line
 
 console.log(arr3);
+
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const {today} = HIGH_TEMPERATURES ;
+  const {tomorrow} = HIGH_TEMPERATURES;
